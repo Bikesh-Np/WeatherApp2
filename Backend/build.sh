24b9a77c4bd71dc3c3ea35005f53319f94f969ad
Bikesh <np03cs4a220443@heraldcollege.edu.np>
2025-05-21 17:08:32 +0545

    Views bug fixed

--- Backend/build.sh
+++ Backend/build.sh
@@ -9,5 +9,13 @@ python manage.py migrate
 
 if [[ $CREATE_SUPERUSER ]]
 then
-    python manage.py createsuperuser --no-input
+    python manage.py createsuperuser \
+        --noinput \
+        --username "$DJANGO_SUPERUSER_USERNAME" \
+        --email "$DJANGO_SUPERUSER_EMAIL" \
+        --first_name "$DJANGO_SUPERUSER_FIRSTNAME" \
+        --last_name "$DJANGO_SUPERUSER_LASTNAME" \
+        --dob "$DJANGO_SUPERUSER_DOB" \
+        --phone "$DJANGO_SUPERUSER_PHONE" \
+        --citizenship "$DJANGO_SUPERUSER_CITIZENSHIP"
 fi
