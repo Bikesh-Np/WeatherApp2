c5c7a04115f9e505ebfb7084c0b47328e41a7538
Bikesh <np03cs4a220443@heraldcollege.edu.np>
2025-05-21 19:30:34 +0545

    Views bug fixed

--- Backend/build.sh
+++ Backend/build.sh
@@ -7,15 +7,15 @@ python manage.py collectstatic --no-input
 python manage.py migrate
 
 
-if [[ $CREATE_SUPERUSER ]]
-then
-    python manage.py createsuperuser \
-        --noinput \
-        --username "$DJANGO_SUPERUSER_USERNAME" \
-        --email "$DJANGO_SUPERUSER_EMAIL" \
-        --first_name "$DJANGO_SUPERUSER_FIRSTNAME" \
-        --last_name "$DJANGO_SUPERUSER_LASTNAME" \
-        --dob "$DJANGO_SUPERUSER_DOB" \
-        --phone "$DJANGO_SUPERUSER_PHONE" \
-        --citizenship "$DJANGO_SUPERUSER_CITIZENSHIP"
-fi
+# if [[ $CREATE_SUPERUSER ]]
+# then
+#     python manage.py createsuperuser \
+#         --noinput \
+#         --username "$DJANGO_SUPERUSER_USERNAME" \
+#         --email "$DJANGO_SUPERUSER_EMAIL" \
+#         --first_name "$DJANGO_SUPERUSER_FIRSTNAME" \
+#         --last_name "$DJANGO_SUPERUSER_LASTNAME" \
+#         --dob "$DJANGO_SUPERUSER_DOB" \
+#         --phone "$DJANGO_SUPERUSER_PHONE" \
+#         --citizenship "$DJANGO_SUPERUSER_CITIZENSHIP"
+# fi
