7f7f20419977960a8afc26c23ad3128c97877610
Bikesh <np03cs4a220443@heraldcollege.edu.np>
2025-05-22 10:19:32 +0545

    Views bug fixed

--- Backend/project/settings.py
+++ Backend/project/settings.py
@@ -109,19 +109,18 @@ CORS_ALLOWED_ORIGINS = [
 CORS_ALLOW_CREDENTIALS = True
 
 # EMAIL
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = 'smtp.gmail.com' 
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = config('EMAIL_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_PASSWORD')
-DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', default=EMAIL_HOST_USER)
+EMAIL_HOST_USER = 'resqlinkmanagement@gmail.com'  
+EMAIL_HOST_PASSWORD = 'pvzq wqrr gezj bnpz' 
 
-# TWILIO
+# Twilio Configurations
 TWILIO_ACCOUNT_SID = config('TWILIO_ACCOUNT_SID')
 TWILIO_AUTH_TOKEN = config('TWILIO_AUTH_TOKEN')
 TWILIO_PHONE_NUMBER = config('TWILIO_PHONE_NUMBER')
-
 # LOGGING
 LOGGING = {
     'version': 1,
